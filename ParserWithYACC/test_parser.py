6b98809939581f8722e45630de01cb26e325c6a6
shein <andrey.shein.spb@gmail.com>
2020-10-07 17:49:07 +0300

    modify tests

--- ParserWithYACC/test_parser.py
+++ ParserWithYACC/test_parser.py
@@ -2,16 +2,14 @@
 import parser
 
 
-def test_integrate_empty_file(tmp_path, monkeypatch, capsys):
+def test_integrate_empty_file(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == ''
 
 
-def test_integrate_good_file(tmp_path, monkeypatch, capsys):
+def test_integrate_good_file(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f :- g. x :- y.\n'
                                     'f :- g, (h; t).\n'
@@ -21,8 +19,6 @@ def test_integrate_good_file(tmp_path, monkeypatch, capsys):
                                     'f :- a, b, c.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'f\n' \
                                         'DEFINITION (f) (g)\n' \
                                         'DEFINITION (x) (y)\n' \
@@ -32,109 +28,89 @@ def test_integrate_good_file(tmp_path, monkeypatch, capsys):
                                         'DEFINITION (f) (AND (a) (AND (b) (c)))\n'
 
 
-def test_integrate_specific_good_file(tmp_path, monkeypatch, capsys):
+def test_integrate_specific_good_file(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f (g).\n'
                                     'f ((g)).\n'
                                     'f :- ((g)).')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'f (g)\n' \
                                         'f ((g))\n' \
                                         'DEFINITION (f) (g)\n'
 
 
-def test_integrate_no_dot1(tmp_path, monkeypatch, capsys):
+def test_integrate_no_dot1(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "f"\n'
 
 
-def test_integrate_no_dot2(tmp_path, monkeypatch, capsys):
+def test_integrate_no_dot2(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f :- g')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "f :- g"\n'
 
 
-def test_integrate_no_head(tmp_path, monkeypatch, capsys):
+def test_integrate_no_head(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     ':- f.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in ":- f."\n'
 
 
-def test_integrate_no_body(tmp_path, monkeypatch, capsys):
+def test_integrate_no_body(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f :- .\n'
                                     'x :- y.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "f :- ."\n'
 
 
-def test_integrate_right_part(tmp_path, monkeypatch, capsys):
+def test_integrate_right_part(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f :- g; h, .\n'
                                     'x :- y.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "f :- g; h, ."\n'
 
 
-def test_integrate_unbalanced_paren(tmp_path, monkeypatch, capsys):
+def test_integrate_unbalanced_paren(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f :- (g; (f).\n'
                                     'x :- y.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "f :- (g; (f)."\n'
 
 
-def test_integrate_empty(tmp_path, monkeypatch, capsys):
+def test_integrate_empty(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     'f ().\n'
                                     'x :- y.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "f ()."\n'
 
 
-def test_integrate_head_in_brackets(tmp_path, monkeypatch, capsys):
+def test_integrate_head_in_brackets(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('f.\n'
                                     '(a) :- f.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "(a) :- f."\n'
 
 
-def test_integrate_wrong_head(tmp_path, monkeypatch, capsys):
+def test_integrate_wrong_head(tmp_path, monkeypatch):
     (tmp_path / 'a.txt').write_text('g, h :- g.')
     monkeypatch.chdir(tmp_path)
     parser.main('a.txt')
-    out, err = capsys.readouterr()
-    assert err == ''
     assert open('a.out', 'r').read() == 'There is a problem in "g, h :- g."\n'
 
 
